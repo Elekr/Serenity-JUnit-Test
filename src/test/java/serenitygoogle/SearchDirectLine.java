b08524faa93341ee418bb0bddb966f6d3c4564dc
Tom <thomascrosby3@gmail.com>
2022-03-04 09:27:40 +0000

    started implementation of direct line automation

--- src/test/java/serenitygoogle/SearchDirectLine.java
+++ src/test/java/serenitygoogle/SearchDirectLine.java
@@ -4,13 +4,26 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.core.steps.UIInteractions;
 import net.thucydides.core.annotations.Step;
+import net.thucydides.core.annotations.WhenPageOpens;
 
 public class SearchDirectLine extends UIInteractions {
 
+    //Creating objects from the webpage to interact with
     @FindBy(className="section__heading")
     private WebElementFacade section__heading;
 
-    @Step("Navigate to home page")
+    @FindBy(id="regSearch")
+    private WebElementFacade regSearch;
+
+    @FindBy(className = "vehicleModified-No")
+    private WebElementFacade vehModNo;
+
+    @FindBy(className = "dlg-hint__text")
+    private WebElementFacade dlgHintText;
+
+
+
+    @Step("Navigate to Home Page")
     public void toHomePage(){
         openUrl("https://www.directline.com/car/quote/your-car");
     }
@@ -19,4 +32,20 @@ public class SearchDirectLine extends UIInteractions {
     public String getFirstHeading(){
         return section__heading.getText();
     }
+
+    @Step("Enter Registration")
+    public void enterRegistration(){regSearch.sendKeys("PR24 BRW");}
+
+    @Step("Interact with Information Button")
+    public String interactInformationButton(){
+
+        vehModNo.click();
+        element(dlgHintText).waitUntilVisible();
+        return dlgHintText.getText();
+    }
+
+//    @WhenPageOpens
+//    public void waitUntilButtonAppears() {
+//        element(vehModNo).waitUntilVisible();
+//    }
 }
--- src/test/java/serenitygoogle/WhenLoggingOn.java
+++ src/test/java/serenitygoogle/WhenLoggingOn.java
@@ -3,7 +3,8 @@ package serenitygoogle;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 
 import net.thucydides.core.annotations.Managed;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
@@ -15,12 +16,26 @@ public class WhenLoggingOn {
     @Managed(driver = "chrome", options = "headless")
     WebDriver driver;
 
-    //Page object set up to hold navigation of the site
+    //Page object set up to hold navigation of the site (Serenity automatically creates an instance)
     SearchDirectLine searchDL;
+
+    @BeforeEach
+    public void BeforeEach(){
+        //Navigate to the DirectLine Homepage
+        searchDL.toHomePage();
+    }
 
     @Test
     public void confirmingCorrectPage(){
-        searchDL.toHomePage();
-        Assert.assertEquals("Heading doesn't match","About the car", searchDL.getFirstHeading());
+        //Check that the website is correct
+        Assertions.assertEquals("About the car", searchDL.getFirstHeading(), "Heading doesn't match");
+    }
+
+    @Test
+    public void interactingWithSite(){
+        searchDL.enterRegistration();
+
+        String text = searchDL.interactInformationButton();
+        System.out.println(text);
     }
 }
